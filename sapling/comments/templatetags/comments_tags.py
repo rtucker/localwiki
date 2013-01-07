5be31f5da250fb4650cd5d57824d9ec9211c5318
Ryan Tucker <rtucker@gmail.com>
2013-01-06 20:53:25 -0500

    Comment form tag fixes
    - Don't use get_or_create -- it will do a Recent Changes event when
      CommentConfiguration is created!  Annoying and a breach of privacy.
    
    - Don't bother injecting a form if it isn't a Page instance (e.g.
      Page_hist)

--- sapling/comments/templatetags/comments_tags.py
+++ sapling/comments/templatetags/comments_tags.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 
 from comments.forms import CommentForm
 from comments.models import CommentConfiguration
+from pages.models import Page
 
 register = template.Library()
 
@@ -19,10 +20,15 @@ def comment_list(page):
 @register.simple_tag(takes_context=True)
 def comment_form(context, page):
     # Output a form for entering a comment, if comments are enabled.
-    cfg, created = CommentConfiguration.objects.get_or_create(page=page)
     context.push()
-    context['comments_enabled'] = cfg.enabled
-    context['form'] = CommentForm(comment_config=cfg)
+    if isinstance(page, Page):
+        try:
+            cfg = CommentConfiguration.objects.get(page=page)
+        except CommentConfiguration.DoesNotExist:
+            cfg = CommentConfiguration(page=page)
+            cfg.save(track_changes=False)
+        context['comments_enabled'] = cfg.enabled
+        context['form'] = CommentForm(comment_config=cfg)
     rendered = render_to_string('comments/comment_form_snippet.html', context)
     context.pop()
     return rendered
